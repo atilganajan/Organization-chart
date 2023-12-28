1e319e7de09a6ce4f6ea1190a5e033200a9d6d7a
Ahmet ATILGAN <atilganajan@gmail.com>
2023-12-28 21:43:02 +0300

    add: Add Department and Person models with relationships

---
+++ app/Models/Department.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function parentDepartment()
+    {
+        return $this->belongsTo(Department::class, 'parent_department_id');
+    }
+
+    public function subDepartments()
+    {
+        return $this->hasMany(Department::class, 'parent_department_id');
+    }
+
+    public function persons()
+    {
+        return $this->hasMany(Person::class);
+    }
+}
