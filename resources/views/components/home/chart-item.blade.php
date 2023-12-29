b04f160720a5aad686d77e0af16a7551133dbc97
Ahmet ATILGAN <atilganajan@gmail.com>
2023-12-29 12:49:44 +0300

    add: Image for chart

--- resources/views/components/home/chart-item.blade.php
+++ resources/views/components/home/chart-item.blade.php
@@ -7,7 +7,7 @@
             @if($subDepartment->persons->isNotEmpty())
                 <ul class="list-group">
                     @foreach($subDepartment->persons as $person)
-                        <li class="list-group-item">{{ $person->name }}</li>
+                        <li class="list-group-item">@if($person->photo )<img src="{{$person->photo}}" style="width: 40px">@else <i class="fas fa-user-circle" style="font-size: 40px;"></i> @endif {{$person->position}} ({{ $person->name }} {{ $person->surname }}</li>
                     @endforeach
                 </ul>
             @endif
